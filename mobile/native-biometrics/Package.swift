88437275847e101dcf03cda1e43ea6749b8b3b5b
ngallodev <49315035+ngallodev@users.noreply.github.com>
2026-09-24 19:33:40 -0700

    Add first-party native biometric plugin

---
+++ mobile/native-biometrics/Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CEWNativeBiometrics",
+    platforms: [.iOS(.v15)],
+    products: [.library(name: "CEWNativeBiometrics", targets: ["NativeBiometricsPlugin"])],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+    ],
+    targets: [
+        .target(
+            name: "NativeBiometricsPlugin",
+            dependencies: [.product(name: "Capacitor", package: "capacitor-swift-pm")],
+            path: "ios/Sources/NativeBiometricsPlugin"
+        )
+    ]
+)
